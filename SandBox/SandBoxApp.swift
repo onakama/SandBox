b567d8f96d0be2cb2f31ff37e1981e700798cdcd
taisei <ninzin60en.dev@gmail.com>
2022-07-16 14:13:52 +0900

    Initial Commit

---
+++ SandBox/SandBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandBoxApp.swift
+//  SandBox
+//
+//  Created by onakama on 2022/07/16.
+//
+
+import SwiftUI
+
+@main
+struct SandBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
