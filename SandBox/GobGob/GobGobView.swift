d9a8375ba16603c7336c065d8869384d18c6430d
taisei <ninzin60en.dev@gmail.com>
2022-07-16 15:59:25 +0900

    add-gob gob

---
+++ SandBox/GobGob/GobGobView.swift
@@ -0,0 +1,22 @@
+//
+//  GobGobView.swift
+//  SandBox
+//
+//  Created by onakama on 2022/07/16.
+//
+
+import SwiftUI
+
+struct GobGobView: View {
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GobGobView_Previews: PreviewProvider {
+    static var previews: some View {
+        GobGobView()
+    }
+}
+
